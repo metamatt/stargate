4fc1a01e196617de905a29e07dac3ef6741149a5
Matt Ginzton <matt@ginzton.net>
2012-08-01 17:52:44 -0700

    Add device-state checkpointing.
    Checkpoint current on/off time from device-state table into history
    tables, periodically or on demand (like exit).
    
    unchanged_since_ts became last_tallied_ts, which is both more useful
    and easier to understand. It gets bumped whenever we tally time from
    the current window into historical windows, and also whenever a
    device changes state (because that causes the former tallying).

--- persistence.py
+++ persistence.py
@@ -11,7 +11,7 @@
 # _device_ _name_ is _state_ since _changetime_, _numchanges_ in _timebucket_, _interesting_state_ for _time_in_state_ ...
 #
 # Bugs/work items:
-# - periodic checkpoints will need difference between last-checkpointed time and last-changed time?
+# - last_tallied_ts may fit more naturally in the history bucket, not the device-status table?
 # - not tracking average level while on yet (and might need more parameters/persisted fields to do so)
 # - bucket rollover, not implemented yet
 # - week bucket doesn't fit the design well
@@ -45,7 +45,6 @@ class SgPersistence(object):
 		
 		self._install_signal_handlers()
 		self._dbfilename = dbconfig['datafile']
-		self._checkpoint_interval = float(dbconfig['checkpoint_interval'])
 		self._conn = sqlite3.connect(self._dbfilename, check_same_thread = False)
 		self._conn.row_factory = sqlite3.Row
 		self._cursor = self._conn.cursor()
@@ -53,7 +52,9 @@ class SgPersistence(object):
 		self._init_schema()
 		self._clear_transient_values()
 		self._lock = threading.RLock()
-		self._install_periodic_checkpointer()
+		self._checkpoint_interval = float(dbconfig['checkpoint_interval'])
+		if self._checkpoint_interval > 0:
+			self._install_periodic_checkpointer()
 
 	# public interface
 	# XXX: should public methods work directly on sg_devid instead of (gateway_id, gateway_devid) pairs?
@@ -81,54 +82,38 @@ class SgPersistence(object):
 			c = self._cursor
 			dev_id = self.get_device_id(gateway_id, gateway_device_id)
 			current_ts = datetime.datetime.now()
-			c.execute('UPDATE device_status SET unchanged_since_ts = ?, last_state = ? WHERE sg_device_id = ?', (current_ts.isoformat(), state, dev_id))
+			c.execute('INSERT OR REPLACE INTO device_status(sg_device_id, last_tallied_ts, last_state) VALUES(?,?,?)',
+			           (dev_id, current_ts.isoformat(), state))
 			self._commit()
 
 	def on_device_state_change(self, gateway_id, gateway_device_id, state):
 		# XXX TODO: handle partial-on states (on_details)
 		with self._lock:
-			c = self._cursor
 			dev_id = self.get_device_id(gateway_id, gateway_device_id)
 			# query device previous state
-			c.execute('SELECT last_state FROM device_status WHERE sg_device_id = ?', (dev_id, ))
-			row = c.fetchone()
-			if row:
-				prev_state = row['last_state']
-			else:
-				prev_state = -1
+			prev_state = self._get_device_prev_state(dev_id)
 			# update last-change table
-			delta, ts_current = self._get_device_change_delta(dev_id) # want to use current-run-only
-			time_in_prev_state = delta.total_seconds() if delta else 0
-			c.execute('INSERT OR REPLACE INTO device_status VALUES(?,?,?,?)', (dev_id, ts_current.isoformat(), ts_current.isoformat(), state))
+			delta, ts_current = self._get_device_timedelta(dev_id, 'last_tallied') # want to use current-run-only
+			c = self._cursor
+			c.execute('INSERT OR REPLACE INTO device_status(sg_device_id, last_seen_event_ts, last_state) VALUES(?,?,?)', (dev_id, ts_current.isoformat(), state))
 			# update history bucket
-			history = self._get_history_bucket(dev_id, 1)
-			on_time, off_time = (history['on_time'], history['off_time'])
-			if prev_state == 0: # was off, tally some off_time
-				logger.debug('adding %g to %g of off_time for did %d' % (time_in_prev_state, off_time, dev_id))
-				off_time += time_in_prev_state
-			elif prev_state > 0: # was on, tally some on_time
-				logger.debug('adding %g to %g of on_time for did %d' % (time_in_prev_state, on_time, dev_id))
-				on_time += time_in_prev_state
-			else:
-				logger.debug('ignoring %g of time in unknown state for did %d' % (time_in_prev_state, dev_id))
-			# note that we ignore any changes from prev_state == -1
-			num_changes = history['num_changes'] + 1
-			c.execute('INSERT OR REPLACE INTO change_history_buckets(sg_device_id, bucket_id, num_changes, on_time, off_time) VALUES(?,?,?,?,?)',
-			          (dev_id, 1, num_changes, on_time, off_time))
+			self._tally_device_state_history(dev_id, prev_state, delta, ts_current, True)
 			# commit
 			self._commit()
-		
+
 	def checkpoint_device_state(self, gateway_id, gateway_device_id):
 		# This is a way of updating the tables used by on_device_state_change for time passing without changes while we are running
-		# XXX TODO, along with bucket rollover
 		with self._lock:
-			pass
-		
+			dev_id = self.get_device_id(gateway_id, gateway_device_id)
+			self._checkpoint_device_state(dev_id)
+			# commit
+			self._commit()
+
 	def get_delta_since_change(self, gateway_id, gateway_device_id):
 		# get time (in seconds) since device registered a change, or None if not known (not since startup)
 		with self._lock:
 			dev_id = self.get_device_id(gateway_id, gateway_device_id)
-			delta, ts_current = self._get_device_change_delta(dev_id, True) # allow timestamp reuse across restarts
+			delta, ts_current = self._get_device_timedelta(dev_id, 'last_seen_event') # allow timestamp reuse across restarts
 			return delta
 	
 	def get_action_count(self, gateway_id, gateway_device_id, bucket):
@@ -143,13 +128,14 @@ class SgPersistence(object):
 		with self._lock:
 			c = self._cursor
 			dev_id = self.get_device_id(gateway_id, gateway_device_id)
+			# Find previous time-in-state (already billed and accounted in historical bucket table)
 			history = self._get_history_bucket(dev_id, bucket)
 			seconds_in_state = history['on_time' if state else 'off_time']
-			# That counts only up to the last change; also add time from then till now if it's still in that state.
+			# Add current time-in-state (historical time counts only up to the last change; add time from them till now if it's still in that state)
 			c.execute('SELECT last_state FROM device_status WHERE sg_device_id = ?', (dev_id, ))
 			row = c.fetchone()
 			if row and row['last_state'] == state:
-				delta, ts_current = self._get_device_change_delta(dev_id) # want to use current-run-only
+				delta, ts_current = self._get_device_timedelta(dev_id, 'last_tallied')
 				seconds_in_state += delta.total_seconds()
 			return datetime.timedelta(seconds = seconds_in_state)
 		
@@ -161,8 +147,62 @@ class SgPersistence(object):
 			return row['bucket_name']
 
 	# private helpers
-	def _checkpoint(self):
-		logger.warn('database checkpoint requested (TODO)')
+	def _checkpoint_all(self):
+		logger.warn('database checkpoint requested')
+		with self._lock:
+			c = self._cursor
+			c.execute('SELECT sg_device_id FROM device_map')
+			for row in c.fetchall():
+				dev_id = row[0]
+				self._checkpoint_device_state(dev_id)
+			self._commit()
+
+	def _checkpoint_device_state(self, dev_id):
+		# Internal helper function: requires lock, does not commit; caller must take care of locking and committing
+		assert self._lock._is_owned() # XXX I want a way to check if owned *by me*!
+		# query device previous state
+		prev_state = self._get_device_prev_state(dev_id)
+		# get amount of untallied time
+		delta, ts_current = self._get_device_timedelta(dev_id, 'last_tallied')
+		# update history bucket
+		self._tally_device_state_history(dev_id, prev_state, delta, ts_current, False)
+		# note: no commit
+		
+	def _tally_device_state_history(self, dev_id, prev_state, delta, ts_current, changed):
+		# Internal helper function: requires lock, does not commit; caller must take care of locking and committing
+		assert self._lock._is_owned() # XXX I want a way to check if owned *by me*!
+		# update history bucket
+		time_in_prev_state = delta.total_seconds() if delta else 0
+		history = self._get_history_bucket(dev_id, 1)
+		on_time, off_time = (history['on_time'], history['off_time'])
+		if prev_state == 0: # was off, tally some off_time
+			logger.debug('adding %g sec to %g sec of off_time for did %d' % (time_in_prev_state, off_time, dev_id))
+			off_time += time_in_prev_state
+		elif prev_state > 0: # was on, tally some on_time
+			logger.debug('adding %g sec to %g sec of on_time for did %d' % (time_in_prev_state, on_time, dev_id))
+			on_time += time_in_prev_state
+		else: # note that we ignore any changes from prev_state == -1
+			logger.debug('ignoring %g sec in unknown state for did %d' % (time_in_prev_state, dev_id))
+		num_changes = history['num_changes']
+		if changed:
+			num_changes += 1
+		c = self._cursor
+		c.execute('INSERT OR REPLACE INTO change_history_buckets(sg_device_id, bucket_id, num_changes, on_time, off_time) VALUES(?,?,?,?,?)',
+		          (dev_id, 1, num_changes, on_time, off_time))
+		# update last-tallied time in device-state table
+		c.execute('UPDATE device_status SET last_tallied_ts = ? WHERE sg_device_id = ?', (ts_current, dev_id))
+		# note: no commit
+		
+	def _get_device_prev_state(self, dev_id):
+		assert self._lock._is_owned() # XXX I want a way to check if owned *by me*!
+		c = self._cursor
+		c.execute('SELECT last_state FROM device_status WHERE sg_device_id = ?', (dev_id, ))
+		row = c.fetchone()
+		if row:
+			prev_state = row['last_state']
+		else:
+			prev_state = -1
+		return prev_state
 
 	def _commit(self):
 		self._conn.commit()
@@ -170,13 +210,11 @@ class SgPersistence(object):
 	def _ts_from_string(self, ts_string):
 		return dateutil.parser.parse(ts_string)
 
-	def _get_device_change_delta(self, device_id, cross_restart = False):
+	def _get_device_timedelta(self, device_id, column_name_base):
 		# Returns tuple: delta since last change to device, and 'now' value used in that calculation, useful for atomicity
 		current_ts = datetime.datetime.now()
 		c = self._cursor
-		# If cross_restart is true, we can use last_seen_event_ts (which may be from a previous run, with unknown intervening data);
-		# if false, we use unchanged_since_ts (which is always from this run, and will return None if it doesn't know).
-		ts_column = 'last_seen_event_ts' if cross_restart else 'unchanged_since_ts'
+		ts_column = column_name_base + '_ts'
 		c.execute('SELECT %s FROM device_status WHERE sg_device_id = ?' % ts_column, (device_id, )) # verified-safe use of % string replacement in SQL query
 		row = c.fetchone()
 		if row and row[0]:
@@ -219,9 +257,9 @@ class SgPersistence(object):
 			
 	def _clear_transient_values(self):
 		# Clear out fields which are not intended to persist beyond the lifetime of a single process.
-		# XXX arguably this stuff should live in the object model instead of the persistence layer...
+		# (XXX: arguably such stuff should live only in the object model and not in the persistence layer....)
 		c = self._cursor
-		c.execute('UPDATE device_status SET unchanged_since_ts = NULL, last_state = -1')
+		c.execute('UPDATE device_status SET last_tallied_ts = NULL, last_state = -1')
 		self._commit()
 
 	def _create_schema(self):
@@ -237,9 +275,10 @@ class SgPersistence(object):
 
 		-- device status for time-in-state tracking
 		-- last_seen_event_ts: last time we saw device change (persists across restart, there may have been newer changes we didn't see) -- useful to say 'last known event''
-		-- unchanged_since_ts: last time we saw device change, this run only (reset to now on startup; can authoritatively say this is true) -- useful to accurately track time windows
+		-- last_tallied_ts: last time we tallied current state (this table) into history buckets (change_history_buckets table) -- reset at process start, so authoritative
 		-- last_state: last known device state, this run only (state as of the last change)
-		CREATE TABLE device_status (sg_device_id INTEGER PRIMARY KEY, last_seen_event_ts STRING, unchanged_since_ts STRING, last_state INTEGER, FOREIGN KEY(sg_device_id) REFERENCES device_map(sg_device_id));
+		CREATE TABLE device_status (sg_device_id INTEGER PRIMARY KEY, last_seen_event_ts STRING, last_tallied_ts STRING, last_state INTEGER,
+		                            FOREIGN KEY(sg_device_id) REFERENCES device_map(sg_device_id));
 
 		-- history buckets
 		CREATE TABLE bucket_defs (bucket_id INTEGER, bucket_name STRING, bucket_description STRING);
@@ -264,7 +303,7 @@ class SgPersistence(object):
 	def _install_signal_handlers(self):
 		def handle_signal(signum, stack_frame):
 			logger.warn("Received signal %d" % signum)
-			self._checkpoint()
+			self._checkpoint_all()
 			if signum != signal.SIGHUP:
 				logger.warn("Exiting on signal %d" % signum)
 				sys.exit()
@@ -276,18 +315,20 @@ class SgPersistence(object):
 	def _install_periodic_checkpointer(self):
 		def checkpoint_callback(self):
 			# invoke the checkpoint
-			self._checkpoint()
+			self._checkpoint_all()
 			# and reinstall this one-shot timer
 			self._install_periodic_checkpointer()
 		self._checkpoint_thread = threading.Timer(self._checkpoint_interval, checkpoint_callback, args = [self])
 		self._checkpoint_thread.setDaemon(True)
+		self._checkpoint_thread.setName('db_cp_timer')
 		self._checkpoint_thread.start()
 
 
 # simple unit test
 def main():
 	logging.basicConfig(level = logging.DEBUG)
-	p = SgPersistence('foo.sqlite')
+	dbconfig = { 'datafile': 'foo.sqlite', 'checkpoint_interval': 1 }
+	p = SgPersistence(dbconfig)
 	id = p.get_device_id('lutron', '24')
 	print id
 	id = p.get_device_id('lutron', '24')
@@ -301,7 +342,7 @@ def main():
 	id = p.get_device_id('lutron', '24')
 	print id
 	p.init_device_state('lutron', '24', 0)
-	time.sleep(0.777) # untracked time
+	time.sleep(0.777) # off time
 	p.on_device_state_change('lutron', '24', 1)
 	time.sleep(3.5) # on time
 	p.on_device_state_change('lutron', '24', 0)
