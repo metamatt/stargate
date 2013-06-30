3341b9258c42724e2a3dfc2d71ed068016e4d5fb
Matt Ginzton <matt@ginzton.net>
2013-06-30 13:53:39 -0700

    Rewrite dsc_reflector, especially to handle multiple child connections.
    The motivation was to handle bug #9, but this was really a bunch of untested
    broken code and it needed changes beyond that.
    
    I took the easy way out and added a new thread for each child connection;
    there shouldn't be many child listeners active anyway.

--- gateways/powerseries/dsc_reflector.py
+++ gateways/powerseries/dsc_reflector.py
@@ -13,6 +13,7 @@
 
 import connections
 import logging
+import select
 import socket
 import threading
 
@@ -21,35 +22,40 @@ logger.info('%s: init with level %s' % (logger.name, logging.getLevelName(logger
 
 
 class ReflectorThread(threading.Thread):
-	def __init__(self, reflector):
+	def __init__(self, parent, socket, client_address):
 		super(ReflectorThread, self).__init__(name = 'dsc_reflector')
 		self.daemon = True
-		self.reflector = reflector
-		self.listen_socket = reflector.accept_socket
+		self.parent = parent
+		self.reflector = parent.reflector
+		self.connected_socket = socket
+		self.client_address = client_address
 		self.authenticated = False
-		
+
 	def run(self):
-		# For now, just listen for a single connection. That's all the envisalink does anyway. Really we should
-		# allow multiple connections (and garbage collect closed ones).
-		# XXX but right now, we don't even listen again if the first one closes.
-		self.connected_socket, self.client_address = self.listen_socket.accept()
-		logger.info('reflector accepted chained connection from %s' % str(self.client_address))
 		self.connected_socket.send('5053CD\r\n') # XXX hardcoded "authentication required" introduction
 		s = self.connected_socket
 		buffer = connections.CrlfSocketBuffer(s)
 
-		while True:
-			(readable, writable, errored) = select.select([s], [], [s])
-			assert readable == [s] # XXX only handle one client for now
-			for line in buffer.read_lines():
-				# XXX we should crack the command, check the checksum, ignore if invalid instead of spamming other clients
-				if line[:3] == '005':
-					auth_response = self.attempt_auth(line)
-					s.send(auth_response + '\r\n')
-				elif self.authenticated:
-					self.reflector.from_child(line)
-				else:
-					logger.warning('DSC reflector: child attempted command %s in unauthenticated state' % line[:3])
+		try:
+			while True:
+				(readable, writable, errored) = select.select([s], [], [s])
+				if errored:
+					logger.warning('reflector child reported error in select()')
+					break
+				assert readable == [s]
+				for line in buffer.read_lines():
+					# XXX we should crack the command, check the checksum, ignore if invalid instead of spamming other clients
+					if line[:3] == '005':
+						auth_response = self.attempt_auth(line)
+						s.send(auth_response + '\r\n')
+					elif self.authenticated:
+						self.reflector.from_child(line)
+					else:
+						logger.warning('DSC reflector: child attempted command %s in unauthenticated state' % line[:3])
+		except:
+			logger.exception('error in reflector child')
+
+		self.parent.child_exit(self)
 	
 	def attempt_auth(self, line):
 		# XXX: should be more careful with state machine, i.e. multiple auth commands. See what real one does and if it matters.
@@ -62,6 +68,41 @@ class ReflectorThread(threading.Thread):
 			return '5050CA' # XXX hardcoded authentication failure response
 
 
+class ReflectorParentThread(threading.Thread):
+	def __init__(self, reflector):
+		super(ReflectorParentThread, self).__init__(name = 'dsc_reflector_listen')
+		self.daemon = True
+		self.reflector = reflector
+		self.listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.listen_socket.bind(('', self.reflector.port))
+		self.listen_socket.listen(1)
+		self.children = []
+
+	def run(self):
+		# Listen for connections, and handle them in new threads
+		while True:
+			connected_socket, client_address = self.listen_socket.accept()
+			logger.info('reflector accepted chained connection from %s' % str(client_address))
+			child = ReflectorThread(self, connected_socket, client_address)
+			self.children.append(child)
+			child.run()
+
+	def to_children(self, cmdLine):
+		logger.debug('send to %d children: %s' % (len(self.children), cmdLine))
+		for child in self.children:
+			logger.debug('to child %s' % str(child.client_address))
+			if child.authenticated:
+				try:
+					child.connected_socket.send(cmdLine)
+				except:
+					logger.exception('unable to send to child %s' % str(child.client_address))
+
+	def child_exit(self, child):
+		logger.info('reflector lost chained connection to %s' % str(child.client_address))
+		self.children.remove(child)
+
+
 class Reflector(object):
 	# XXX the envisalink authentication scheme is really lame; we might want to support something
 	# better, and/or at least a different password, and/or at least restrict the listening address
@@ -71,24 +112,20 @@ class Reflector(object):
 		self.gateway = gateway
 		self.port = port
 		self.password = password
+		self.reflect_thread = None
 		
 		if self.port:
-			self.accept_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			self.accept_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-			self.accept_socket.bind(('', self.port))
-			self.accept_socket.listen(1)
-			self.reflect_thread = ReflectorThread(self)
+			self.reflect_thread = ReflectorParentThread(self)
 			self.send_lock = threading.RLock()
 			self.reflect_thread.start()
 			
 	def to_children(self, cmdline):
-		if not self.port:
-			return
-		with self.send_lock:
-			if self.reflect_thread.authenticated:
-				self.reflect_thread.connected_socket.send(cmdline + '\r\n')
+		if self.reflect_thread:
+			with self.send_lock:
+				self.reflect_thread.to_children(cmdline + '\r\n')
 				
 	def from_child(self, cmdline):
 		# Child gave command; pass along to DSC
-		assert cmdline[:3] != '005' # make sure children don't mess with parent authentication state
+		if cmdline[:3] == '005': # make sure children don't mess with parent authentication state
+			return
 		self.gateway.panel_server._send_dsc_cmdline(cmdline)
